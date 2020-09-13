ff1bd410bd856fe63e169a0a4c081800a577c320
bastian <bastianganga@gmail.com>
2020-09-13 01:09:00 -0300

    Plot 1

---
+++ Plot1.R
@@ -0,0 +1,22 @@
+# Load library
+library(data.table)
+
+# download dataset and define path
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+path <- getwd()
+download.file(url, file.path(path, "dataset.zip"))
+unzip(zipfile = "dataset.zip")
+
+# Load dataset
+powercon <- read.table("household_power_consumption.txt", skip = 1, sep = ";")
+names(powercon) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# Filter Date
+subpowercon <- subset(powercon, powercon$Date=="1/2/2007" | powercon$Date =="2/2/2007")
+
+# Plotting
+hist(as.numeric(as.character(subpowercon$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+title(main="Global Active Power")
+
+# png
+png("plot1.png", width=480, height=480)
